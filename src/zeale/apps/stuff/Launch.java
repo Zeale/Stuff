9fc6bb8d0d6130a88d41fb222c2b549dd8e5408d
Zeale <zeale@dusttoash.org>
2019-04-11 22:42:10 -0500

    Added project starting files

---
+++ src/zeale/apps/stuff/Launch.java
@@ -0,0 +1,7 @@
+package zeale.apps.stuff;
+
+public class Launch {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/zeale/apps/stuff/Stuff.java
@@ -0,0 +1,9 @@
+package zeale.apps.stuff;
+
+public class Stuff {
+	public static void main(String[] args) {
+		// The application can be launched from here, but this will just revert to the
+		// Launch class's launch method.
+		Launch.main(args);
+	}
+}
