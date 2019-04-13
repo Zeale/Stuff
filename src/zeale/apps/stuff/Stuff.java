9212efecbaea3dc6220999b99fe9587440944520
Zeale <zeale@dusttoash.org>
2019-04-13 00:46:22 -0500

    Updated Stuff cls

--- src/zeale/apps/stuff/Stuff.java
+++ src/zeale/apps/stuff/Stuff.java
@@ -1,6 +1,16 @@
 package zeale.apps.stuff;
 
+import java.io.File;
+
 public class Stuff {
+
+	/**
+	 * The program's installation directory. This is laxly detected (as of now) by
+	 * simply getting the program's working directory. Some sort of storage API will
+	 * need to be made later.
+	 */
+	private static final File INSTALLATION_DIRECTORY = new File("");
+
 	public static void main(String[] args) {
 		// The application can be launched from here, but this will just revert to the
 		// Launch class's launch method.
