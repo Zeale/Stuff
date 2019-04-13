cdcc00753d1fd534ef91b18ad3ec725369e9515f
Zeale <zeale@dusttoash.org>
2019-04-13 00:45:56 -0500

    Added launch code

--- src/zeale/apps/stuff/Launch.java
+++ src/zeale/apps/stuff/Launch.java
@@ -1,7 +1,22 @@
 package zeale.apps.stuff;
 
-public class Launch {
-	public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
 
+public class Launch extends Application {
+	public static void main(String[] args) {
+		Platform.setImplicitExit(false);
+		launch(Launch.class, args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// When the primary window is closed, we shut down the application. (This
+		// behavior is very likely to change later.
+		/// close exit primary-window
+		primaryStage.setOnCloseRequest(event -> Platform.exit());
+		primaryStage.show();
+		
 	}
 }
