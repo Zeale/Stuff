6c71a0d73a7bc1b396ed90b7671c50d006b42c74
Zeale <zeale@dusttoash.org>
2019-04-13 00:46:37 -0500

    Added a home window class.

---
+++ src/zeale/apps/stuff/app/guis/windows/HomeWindow.java
@@ -0,0 +1,21 @@
+package zeale.apps.stuff.app.guis.windows;
+
+import javafx.stage.Stage;
+import zeale.apps.stuff.api.appprops.ApplicationProperties;
+import zeale.apps.stuff.api.guis.windows.Window;
+
+public class HomeWindow implements Window {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void display(Stage stage, ApplicationProperties properties) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
