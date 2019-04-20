7ae53a207b664d6c4d3791ac10d189fc354db50f
Zeale <zeale@dusttoash.org>
2019-04-20 02:02:36 -0500

    Updated TaggedCalculatorButton

--- src/javafx-nodes/zeale/apps/stuff/api/javafx/guis/windows/calculator/TaggedCalculatorButton.java
+++ src/javafx-nodes/zeale/apps/stuff/api/javafx/guis/windows/calculator/TaggedCalculatorButton.java
@@ -1,16 +1,33 @@
 package zeale.apps.stuff.api.javafx.guis.windows.calculator;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 
 public class TaggedCalculatorButton extends Button {
 
-	{
-		getStyleClass().add("pop-button");
-	}
-
 	private final StringProperty tag = new SimpleStringProperty();
+	private final BooleanProperty tooltipBound = new SimpleBooleanProperty(true);
+
+	{
+
+		getStyleClass().add("pop-button");
+		ChangeListener<String> tagChangeListener = (observable, oldValue,
+				newValue) -> setTooltip(new Tooltip(getTag()));
+
+		tooltipBound.addListener((ChangeListener<Boolean>) (observable, oldValue, newValue) -> {
+			if (newValue)
+				tag.addListener(tagChangeListener);
+			else
+				tag.removeListener(tagChangeListener);
+		});
+		tag.addListener(tagChangeListener);
+		
+	}
 
 	public StringProperty tagProperty() {
 		return tag;
