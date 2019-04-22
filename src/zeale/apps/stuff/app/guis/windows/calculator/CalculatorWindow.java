1fed7e0837d8c678890f7dfc32d93b0bab555da7
Zeale <zeale@dusttoash.org>
2019-04-22 14:02:28 -0500

    Updated search function on calc extended func tab

--- src/zeale/apps/stuff/app/guis/windows/calculator/CalculatorWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/calculator/CalculatorWindow.java
@@ -1,26 +1,74 @@
 package zeale.apps.stuff.app.guis.windows.calculator;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.alixia.libs.evaluator.Evaluator;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
+import zeale.apps.stuff.api.javafx.guis.windows.calculator.TaggedCalculatorButton;
 
 public final class CalculatorWindow extends Window {
 
 	private @FXML Button searchButton;
 	private @FXML TextField inputField;
 
+	private @FXML TextField extendedFunctionalitySearch;
+	private @FXML TilePane extendedFunctionalityTilePane;
+
 	private @FXML void initialize() {
+
+		List<TaggedCalculatorButton> buttons = new ArrayList<>(extendedFunctionalityTilePane.getChildren().size());
+		for (Node n : extendedFunctionalityTilePane.getChildren())
+			if (n instanceof TaggedCalculatorButton)
+				buttons.add((TaggedCalculatorButton) n);
+
+		extendedFunctionalitySearch.textProperty().addListener(new ChangeListener<String>() {
+
+			private boolean matches(String searchTerm, String itemName) {
+				return itemName.toLowerCase().contains(searchTerm.toLowerCase());
+			}
+
+			@Override
+			public synchronized void changed(ObservableValue<? extends String> observable, String oldValue,
+					String newValue) {
+				if (newValue.contains(oldValue) && newValue.length() > oldValue.length())
+					for (Iterator<Node> iterator = extendedFunctionalityTilePane.getChildren().iterator(); iterator
+							.hasNext();) {
+						Node n = iterator.next();
+						if (n instanceof TaggedCalculatorButton
+								&& !matches(newValue, ((TaggedCalculatorButton) n).getTag()))
+							iterator.remove();
+					}
+				else {
+					for (Iterator<Node> iterator = extendedFunctionalityTilePane.getChildren().iterator(); iterator
+							.hasNext();) {
+						Node n = iterator.next();
+						if (n instanceof TaggedCalculatorButton)
+							iterator.remove();
+					}
+					for (TaggedCalculatorButton tcb : buttons)
+						if (matches(newValue, tcb.getTag()))
+							extendedFunctionalityTilePane.getChildren().add(tcb);
+				}
+
+			}
+		});
 
 		try {
 			ImageView searchIcon = new ImageView("/zeale/apps/stuff/rsrc/app/gui/windows/calculator/Search Icon.png");
