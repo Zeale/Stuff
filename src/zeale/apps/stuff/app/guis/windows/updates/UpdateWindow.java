6efa82563a0b844639da42c065c5e0378b21ffff
Zeale <zeale@dusttoash.org>
2019-04-25 02:12:26 -0500

    Added Update Window gui & css file

--- src/zeale/apps/stuff/app/guis/windows/updates/UpdateWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/updates/UpdateWindow.java
@@ -1,10 +1,18 @@
 package zeale.apps.stuff.app.guis.windows.updates;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
 
 public class UpdateWindow extends Window {
+	
+	private @FXML VBox updateSelectorBox;
+
+	private @FXML void initialize() {
+
+	}
 
 	@Override
 	public void destroy() {
