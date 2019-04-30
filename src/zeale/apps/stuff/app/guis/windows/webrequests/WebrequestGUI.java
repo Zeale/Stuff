29e7865c8c01384ee19b192e8630dedc6c89e080
Zeale <zeale@dusttoash.org>
2019-04-30 15:11:57 -0500

    Updated Web Request app

--- src/zeale/apps/stuff/app/guis/windows/webrequests/WebrequestGUI.java
+++ src/zeale/apps/stuff/app/guis/windows/webrequests/WebrequestGUI.java
@@ -1,11 +1,17 @@
 package zeale.apps.stuff.app.guis.windows.webrequests;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.Callable;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -57,15 +63,78 @@ public class WebrequestGUI extends Window {
 		}
 	}
 
-	private @FXML TextArea resultBox, bodyBox, rawBox, errorBox;
+	private @FXML TextArea finalizedRequestBox, bodyBox, responseBox, errorBox;
 	private @FXML WebView renderView;
+	private @FXML TextField urlPrompt, userAgentPrompt, parameterPrompt, methodPrompt;
+	private @FXML Button sendButton;
 
-	private @FXML void send(ActionEvent e) {
+	private final BooleanProperty sendable = new SimpleBooleanProperty();
+
+	private @FXML void initialize() {
+
+		sendButton.disableProperty().bind(sendable);
+
+		finalizedRequestBox.textProperty().bind(Bindings.createStringBinding(new Callable<String>() {
+
+			private boolean sendable = true;
+
+			private String blockSend() {
+				WebrequestGUI.this.sendable.set(sendable = false);
+				return "???";
+			}
+
+			private void unblockSend() {
+				if (sendable)
+					WebrequestGUI.this.sendable.set(true);
+			}
+
+			@Override
+			public String call() throws Exception {
+				sendable = true;
+				synchronized (WebrequestGUI.this) {
+
+					// Request
+					String txt = methodPrompt.getText();
+					if (txt.isEmpty())
+						txt = blockSend();
+					else
+						for (char c : txt.toCharArray())
+							if (Character.isWhitespace(c)) {
+								txt = blockSend();
+								break;
+							}
+					finalizedRequestBox.setText(txt + " HTTP/1.1");
+
+					// TODO Move this stuff to methods.
+					//Query (either 
+					String postContent = null;
+
+					try {
+						URL url = new URL(urlPrompt.getText());
+						finalizedRequestBox.appendText(url.getPath());
+						if (txt.equals("GET"))
+							finalizedRequestBox.appendText(url.getQuery());
+						else
+							postContent = url.getQuery();
+					} catch (Exception e) {
+						finalizedRequestBox.appendText(blockSend());
+					}
+
+					unblockSend();
+
+				}
+				return null;
+			}
+		}, bodyBox.textProperty(), urlPrompt.textProperty(), userAgentPrompt.textProperty(),
+				parameterPrompt.textProperty(), methodPrompt.textProperty()));
+	}
+
+	private @FXML synchronized void send(ActionEvent e) {
 		// TODO Code
 	}
 
 	private @FXML void clearErrorLog(ActionEvent e) {
-		// TODO Code
+		errorBox.clear();
 	}
 
 }
