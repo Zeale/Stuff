22f77e1d8c48a7b328e88cf2253adbe20f37c87d
Zeale <zeale@dusttoash.org>
2019-04-29 23:54:55 -0500

    Updated Web Request GUI (linked to controller and added bit of functionality)

--- src/zeale/apps/stuff/app/guis/windows/webrequests/WebrequestGUI.java
+++ src/zeale/apps/stuff/app/guis/windows/webrequests/WebrequestGUI.java
@@ -1,5 +1,13 @@
 package zeale.apps.stuff.app.guis.windows.webrequests;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
@@ -12,10 +20,28 @@ public class WebrequestGUI extends Window {
 
 	}
 
+	private @FXML TextField requestDialog;
+
+	private @FXML void selectCustomRequest(ActionEvent e) {
+		requestDialog.setDisable(false);
+	}
+
+	private @FXML void selectRequest(ActionEvent e) {
+		requestDialog.setDisable(true);
+		Object source = e.getSource();
+		if (source instanceof MenuItem)
+			requestDialog.setText(((MenuItem) source).getText());
+	}
+
 	@Override
 	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
-		// TODO Auto-generated method stub
-
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("WebrequestGUI.fxml"));
+		loader.setController(this);
+		try {
+			stage.setScene(new Scene(loader.load()));
+		} catch (IOException e) {
+			throw new WindowLoadFailureException("Failed to load the UI for the Web Request Window.", e);
+		}
 	}
 
 }
