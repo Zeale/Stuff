4cd914d2f9ad4429c82f85fcf4d62efd8d32efd0
Zeale <zeale@dusttoash.org>
2019-05-20 18:40:16 -0500

    Added some methods to Menu class

--- src/zeale/apps/stuff/api/guis/windows/Menu.java
+++ src/zeale/apps/stuff/api/guis/windows/Menu.java
@@ -1,17 +1,29 @@
 package zeale.apps.stuff.api.guis.windows;
 
+import java.util.function.Supplier;
+
 import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import krow.fx.scene.HorizontalScrollBox;
+import krow.guis.PopupHelper;
 import main.alixia.javalibrary.javafx.tools.FXTools;
+import zeale.apps.stuff.Stuff;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
+import zeale.apps.stuff.api.logging.Logging;
 
 public class Menu extends Window {
 
@@ -19,6 +31,53 @@ public class Menu extends Window {
 	public void destroy() {
 		// TODO Auto-generated method stub
 
+	}
+
+	public void addItem(Node item) {
+		scrollBox.getChildren().add(item);
+	}
+
+	public void removeItem(Node item) {
+		scrollBox.getChildren().remove(item);
+	}
+
+	public ObservableList<Node> getChildren() {
+		return scrollBox.getChildren();
+	}
+
+	public StackPane addImageNode(String imageURL, EventHandler<? super MouseEvent> onClick, String popup) {
+		return addImageNode(imageURL, onClick, new Label(popup));
+	}
+
+	public StackPane addImageNode(String imageURL, EventHandler<? super MouseEvent> onClick, Label popup) {
+		ImageView icon = new ImageView(new Image(imageURL, -1, 128, true, false));
+		icon.setPreserveRatio(true);
+		icon.setFitHeight(128);
+		icon.setPickOnBounds(true);
+		StackPane box = new StackPane(icon);
+		box.setMinSize(128, 128);
+
+		box.setOnMouseClicked(onClick);
+
+		PopupHelper.applyInstantInfoPopup(box, PopupHelper.buildPopup(popup).popup);
+
+		scrollBox.getChildren().add(box);
+		return box;
+	}
+
+	public StackPane addImageNode(String imageURL, Supplier<Window> windowSupplier, Label popup) {
+		return addImageNode(imageURL, (a) -> {
+			try {
+				Stuff.displayWindow(windowSupplier.get());
+			} catch (WindowLoadFailureException e) {
+				Logging.err("Failed to open the window...\n");
+				Logging.err(e);
+			}
+		}, popup);
+	}
+
+	public StackPane addImageNode(String imageURL, Supplier<Window> windowSupplier, String popup) {
+		return addImageNode(imageURL, windowSupplier, new Label(popup));
 	}
 
 	protected final HorizontalScrollBox scrollBox = new HorizontalScrollBox();
