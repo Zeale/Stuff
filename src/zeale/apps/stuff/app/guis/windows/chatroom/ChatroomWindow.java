1298851bdb324ad664eff2adae687ddeb8748f24
Zeale <zeale@dusttoash.org>
2019-05-20 13:42:22 -0500

    Added Chatroom files

---
+++ src/zeale/apps/stuff/app/guis/windows/chatroom/ChatroomWindow.java
@@ -0,0 +1,26 @@
+package zeale.apps.stuff.app.guis.windows.chatroom;
+
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import zeale.apps.stuff.api.appprops.ApplicationProperties;
+import zeale.apps.stuff.api.guis.windows.Window;
+
+public class ChatroomWindow extends Window {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private @FXML void initialize() {
+		// TODO
+	}
+
+	@Override
+	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
