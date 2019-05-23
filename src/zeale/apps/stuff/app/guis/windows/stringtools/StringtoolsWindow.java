a44c4535dba1c0d051f4f1347198e296ba0edc31
Zeale <zeale@dusttoash.org>
2019-05-22 21:41:19 -0500

    Added Stringtools window files

---
+++ src/zeale/apps/stuff/app/guis/windows/stringtools/StringtoolsWindow.java
@@ -0,0 +1,21 @@
+package zeale.apps.stuff.app.guis.windows.stringtools;
+
+import javafx.stage.Stage;
+import zeale.apps.stuff.api.appprops.ApplicationProperties;
+import zeale.apps.stuff.api.guis.windows.Window;
+
+public class StringtoolsWindow extends Window {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
