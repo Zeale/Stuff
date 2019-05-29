04045f2cebc3f021f57eb2ce84be8ce1cbb14817
Zeale <zeale@dusttoash.org>
2019-05-29 00:23:51 -0500

    Updated Encryption app code

--- src/zeale/apps/stuff/app/guis/windows/encryption/EncryptionWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/encryption/EncryptionWindow.java
@@ -1,5 +1,10 @@
 package zeale.apps.stuff.app.guis.windows.encryption;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
@@ -16,6 +21,24 @@ public class EncryptionWindow extends Window {
 	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
 		// TODO Auto-generated method stub
 
+	}
+
+	private @FXML TextArea inputField, outputField;
+
+	private @FXML void encryptInput() {
+
+	}
+
+	private @FXML void decryptOutput() {
+
+	}
+
+	private static String encrypt(String input) {
+		
+	}
+
+	private static byte[] hashKey(String key, String algorithm) {
+		
 	}
 
 }
