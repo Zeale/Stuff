e31e9593f8dd944fbdde573c21facfd9b3a984ce
Zeale <zeale@dusttoash.org>
2019-06-02 04:01:28 -0500

    Partial drag and drop updates
    For some stupid reason, I can't seem to drag and drop files into the
    app. After some testing, I can drag and drop from one node in an app to
    another node in the same app, but dragging files in doesn't seem to be
    working.
    
    Until I can fix this, I'll probably either work on something else or
    have get workaround so that I can implement module stuff.

--- src/zeale/apps/stuff/app/guis/windows/HomeWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/HomeWindow.java
@@ -1,12 +1,22 @@
 package zeale.apps.stuff.app.guis.windows;
 
+import java.io.File;
+
+import javafx.animation.Interpolator;
+import javafx.animation.Transition;
+import javafx.event.EventHandler;
 import javafx.scene.control.Label;
+import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import zeale.applicationss.notesss.utilities.Utilities;
 import zeale.apps.stuff.Stuff;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Menu;
@@ -15,6 +25,77 @@ import zeale.apps.stuff.app.guis.windows.encryption.EncryptionWindow;
 import zeale.apps.stuff.app.guis.windows.webrequests.WebrequestWindow;
 
 public class HomeWindow extends Menu {
+
+	private final Transition blurTransition = new Transition() {
+
+		{
+			setCycleDuration(Duration.millis(300));
+			setInterpolator(Interpolator.EASE_IN);
+		}
+
+		@Override
+		protected void interpolate(double frac) {
+			anchorPane.setEffect(new GaussianBlur(frac * 23));
+		}
+	};
+
+	{
+		anchorPane.setOnDragOver(event -> {
+			System.out.println("Drag Over");
+
+			if (event.getDragboard().hasFiles())
+				event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+			event.consume();
+		});
+
+		anchorPane.setOnDragEntered(event -> {
+			System.out.println("Drag Entered");
+
+			blurTransition.stop();
+			blurTransition.setRate(1);
+			blurTransition.play();
+			if (event.getDragboard().hasFiles()) {
+				boolean hasOnlyJars = true, hasNoJars = true;
+				for (File f : event.getDragboard().getFiles())
+					if (f.getName().endsWith(".jar"))
+						hasNoJars = false;
+					else
+						hasOnlyJars = false;
+				if (hasOnlyJars)
+					anchorPane.setBackground(Utilities.getBackgroundFromColor(Color.GOLD));
+				else if (hasNoJars)
+					anchorPane.setBackground(Utilities.getBackgroundFromColor(Color.RED));
+				else
+					anchorPane.setBackground(Utilities.getBackgroundFromColor(Color.GREEN));
+			} else
+				anchorPane.setBackground(Utilities.getBackgroundFromColor(Color.FIREBRICK));
+			event.consume();
+		});
+
+		anchorPane.setOnDragExited(event -> {
+			System.out.println("Drag Exited");
+
+			blurTransition.stop();
+			blurTransition.setRate(-1);
+			blurTransition.play();
+			anchorPane.setBackground(Utilities.getBackgroundFromColor(DEFAULT_BACKGROUND_COLOR));
+		});
+
+		anchorPane.setOnDragDropped(new EventHandler<DragEvent>() {
+
+			@Override
+			public void handle(DragEvent event) {
+				System.out.println("Drag Dropped");
+
+				if (event.getDragboard().hasFiles()) {
+					boolean hasJars = false;
+					for (File f : event.getDragboard().getFiles()) {
+
+					}
+				}
+			}
+		});
+	}
 
 	@Override
 	public void destroy() {
