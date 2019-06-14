bd82612c7186ad78d813c2a9e779df054cc3d0dc
Zeale <zeale@dusttoash.org>
2019-06-14 00:38:53 -0500

    Updated task scheduler window

--- src/zeale/apps/stuff/app/guis/windows/taskscheduler/TaskSchedulerWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/taskscheduler/TaskSchedulerWindow.java
@@ -3,6 +3,7 @@ package zeale.apps.stuff.app.guis.windows.taskscheduler;
 import java.io.File;
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,12 +11,26 @@ import zeale.apps.stuff.Stuff;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
 import zeale.apps.stuff.api.logging.Logging;
+import zeale.apps.stuff.utilities.java.references.PhoenixReference;
 import zeale.apps.tools.api.data.files.filesystem.storage.FileStorage;
 
 public class TaskSchedulerWindow extends Window {
 
-	private final static FileStorage TASK_DATA_DIR = new FileStorage(
-			new File(new File(Stuff.INSTALLATION_DIRECTORY, "App Data"), "Task Scheduler")).createChild("Tasks");
+	private final static PhoenixReference<FileStorage> TASK_DATA_DIR = PhoenixReference.create(
+			() -> new FileStorage(new File(new File(Stuff.INSTALLATION_DIRECTORY, "App Data"), "Task Scheduler"))
+					.createChild("Tasks"));
+
+	private @FXML void reload() {
+		/* TODO */
+	}
+
+	private @FXML void update() {
+		/* TODO */
+	}
+
+	private @FXML void flush() {
+		/* TODO */
+	}
 
 	@Override
 	public void destroy() {
