737af95ff79933ab33ddc26581e7608cf8bbddb6
Zeale <zeale@dusttoash.org>
2019-06-14 19:09:50 -0500

    Made Sync CheckBoxes work for task scheduler

--- src/zeale/apps/stuff/app/guis/windows/taskscheduler/TaskSchedulerWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/taskscheduler/TaskSchedulerWindow.java
@@ -3,9 +3,15 @@ package zeale.apps.stuff.app.guis.windows.taskscheduler;
 import java.io.File;
 import java.io.IOException;
 
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import zeale.apps.stuff.Stuff;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
@@ -19,6 +25,25 @@ public class TaskSchedulerWindow extends Window {
 	private final static PhoenixReference<FileStorage> TASK_DATA_DIR = PhoenixReference.create(
 			() -> new FileStorage(new File(new File(Stuff.INSTALLATION_DIRECTORY, "App Data"), "Task Scheduler"))
 					.createChild("Tasks"));
+
+	private @FXML TextField createName, editName;
+	private @FXML TextArea createDescription, editDescription;
+	private @FXML CheckBox createComplete, editComplete, createUrgent, editUrgent, editSync1, editSync2;
+	private @FXML Button editFlush;
+
+	private @FXML void initialize() {
+		editSync1.selectedProperty().bindBidirectional(editSync2.selectedProperty());
+		editSync1.selectedProperty().addListener((ChangeListener<Boolean>) (observable, oldValue, newValue) -> {
+			if (newValue) {
+				AnchorPane.setBottomAnchor(editDescription, 50d);
+				editFlush.setVisible(false);
+			} else {
+				AnchorPane.setBottomAnchor(editDescription, 200d);
+				editFlush.setVisible(true);
+			}
+		});
+		/* TODO */
+	}
 
 	private @FXML void reload() {
 		/* TODO */
