da1f9ecb4e8b13026a6152a616f2a985dccb5dcb
Zeale <zeale@dusttoash.org>
2019-06-14 19:16:28 -0500

    Stole updates from Password Manager branch

--- src/zeale/apps/stuff/api/guis/windows/Menu.java
+++ src/zeale/apps/stuff/api/guis/windows/Menu.java
@@ -9,6 +9,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.effect.Effect;
 import javafx.scene.effect.Glow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -149,9 +150,20 @@ public class Menu extends Window {
 	}
 
 	protected final HorizontalScrollBox scrollBox = new HorizontalScrollBox();
-	protected final Glow hoverGlow = new Glow(1);
+	protected static final Glow hoverGlow = new Glow(1);
 	protected final VBox centerer = new VBox();
 	protected final AnchorPane anchorPane = new AnchorPane(centerer);
+
+	private static final Object CUSTOM_EFFECT_KEY = new Object();
+
+	protected static Effect getCustomEffect(Node node) {
+		return (Effect) node.getProperties().getOrDefault(CUSTOM_EFFECT_KEY, hoverGlow);
+	}
+
+	protected static final void setCustomEffect(Node node, Effect effect) {
+		node.getProperties().put(CUSTOM_EFFECT_KEY, effect);
+	}
+
 	{
 		centerer.setFillWidth(true);
 		centerer.setAlignment(Pos.CENTER);
@@ -164,7 +176,7 @@ public class Menu extends Window {
 				if (c.wasAdded())
 					for (Node n1 : c.getAddedSubList()) {
 						n1.setOnMouseEntered(event1 -> {
-							n1.setEffect(hoverGlow);
+							n1.setEffect(getCustomEffect(n1));
 							n1.setScaleX(1.05);
 							n1.setScaleY(1.05);
 						});
