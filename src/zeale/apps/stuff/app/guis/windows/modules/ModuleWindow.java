d70c6a45de4d9b8589c1573abdfb16e17e86008e
Zeale <zeale@dusttoash.org>
2019-06-19 04:36:15 -0500

    ModuleWindow static management methods

--- src/zeale/apps/stuff/app/guis/windows/modules/ModuleWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/modules/ModuleWindow.java
@@ -1,5 +1,7 @@
 package zeale.apps.stuff.app.guis.windows.modules;
 
+import java.io.File;
+import java.util.Collection;
 import javafx.fxml.FXML;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -17,13 +19,23 @@ public class ModuleWindow extends Window {
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
-		// TODO Auto-generated method stub
+		/* TODO */
+	}
 
+	public static void loadModules(Collection<File> modules) {
+		/* TODO */
+	}
+
+	public static void loadModule(File module) {
+		/* TODO */
+	}
+
+	public static void loadPackage(File pckge) {
+		/* TODO */
 	}
 
 }
