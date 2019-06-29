ab68514751b6ebb292c27191b5f6ba273688df90
Zeale <zeale@dusttoash.org>
2019-06-29 13:43:25 -0500

    Updated Label manager gui

--- src/zeale/apps/stuff/app/guis/windows/taskscheduler/LabelManagerWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/taskscheduler/LabelManagerWindow.java
@@ -1,10 +1,16 @@
 package zeale.apps.stuff.app.guis.windows.taskscheduler;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
 
 public class LabelManagerWindow extends Window {
+
+	private @FXML TilePane labelView;
+	private @FXML TextField labelSearch;
 
 	@Override
 	public void destroy() {
