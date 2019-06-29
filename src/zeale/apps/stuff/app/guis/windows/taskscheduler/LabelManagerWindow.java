1a58eaef17894c6d7895f19b235774c1b94f9cd7
Zeale <zeale@dusttoash.org>
2019-06-29 13:34:20 -0500

    Added LabelManager window files

---
+++ src/zeale/apps/stuff/app/guis/windows/taskscheduler/LabelManagerWindow.java
@@ -0,0 +1,21 @@
+package zeale.apps.stuff.app.guis.windows.taskscheduler;
+
+import javafx.stage.Stage;
+import zeale.apps.stuff.api.appprops.ApplicationProperties;
+import zeale.apps.stuff.api.guis.windows.Window;
+
+public class LabelManagerWindow extends Window {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
