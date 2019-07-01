b90381b3b031231c8722f3820d5d0becb36c00b8
Zeale <zeale@dusttoash.org>
2019-06-30 23:50:03 -0500

    Updated LabelManager GUI and Window

--- src/zeale/apps/stuff/app/guis/windows/taskscheduler/LabelManagerWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/taskscheduler/LabelManagerWindow.java
@@ -1,9 +1,16 @@
 package zeale.apps.stuff.app.guis.windows.taskscheduler;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.SplitPane.Divider;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.TilePane;
@@ -14,8 +21,12 @@ import zeale.apps.stuff.api.logging.Logging;
 import zeale.apps.stuff.app.guis.windows.taskscheduler.TaskSchedulerWindow.NameNotFoundException;
 import zeale.apps.tools.console.std.BindingConversion;
 
-public class LabelManagerWindow extends Window {
+class LabelManagerWindow extends Window {
 
+	private @FXML SplitPane splitPaneWrapper;
+	private Divider split;
+	private @FXML TabPane manipulationPane;
+	private @FXML Tab createTab, modifyTab;
 	private @FXML TilePane labelView;
 	private @FXML TextField labelSearch, createName, modName, createID, modID;
 	private @FXML ColorPicker createColor, modColor;
@@ -23,6 +34,7 @@ public class LabelManagerWindow extends Window {
 
 	private @FXML void initialize() {
 		BindingConversion.bind(TaskSchedulerWindow.LABEL_LIST.get(), LabelView::new, labelView.getChildren());
+		split = splitPaneWrapper.getDividers().get(0);
 	}
 
 	private @FXML void createLabel() {
@@ -35,6 +47,7 @@ public class LabelManagerWindow extends Window {
 			Label lbl = TaskSchedulerWindow.createLabel();
 			lbl.setColor(createColor.getValue());
 			lbl.setDescription(createDesc.getText());
+			lbl.setName(createName.getText());
 			TaskSchedulerWindow.LABEL_LIST.get().add(lbl);
 			lbl.flush();
 		} catch (FileNotFoundException e) {
@@ -42,6 +55,18 @@ public class LabelManagerWindow extends Window {
 		} catch (NameNotFoundException e) {
 			Logging.err("Failed to create the Label. An unused unique ID could not be found.");
 		}
+	}
+
+	public @FXML void showEditMenu() {
+		if (split.getPosition() > 0.95)
+			split.setPosition(0.5);
+		manipulationPane.getSelectionModel().select(modifyTab);
+	}
+
+	public @FXML void showCreateMenu() {
+		if (split.getPosition() > 0.95)
+			split.setPosition(0.5);
+		manipulationPane.getSelectionModel().select(createTab);
 	}
 
 	private @FXML void modifyLabel() {
@@ -56,8 +81,14 @@ public class LabelManagerWindow extends Window {
 
 	@Override
 	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
-		// TODO Auto-generated method stub
-
+		FXMLLoader loader = new FXMLLoader(LabelManagerWindow.class.getResource("LabelManagerGUI.fxml"));
+		loader.setController(this);
+		try {
+			stage.setScene(new Scene(loader.load()));
+		} catch (IOException e) {
+			Logging.err("Failed to load the label manipulation window.");
+			Logging.err(e);
+		}
 	}
 
 }
