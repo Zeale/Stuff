b6a2a31948137109e8ad16790cf058f116ffddfc
Zeale <zeale@dusttoash.org>
2019-07-14 01:13:49 -0500

    Added ParsedObjectCommand (data struct for cmds)

---
+++ src/zeale/apps/stuff/app/console/ParsedObjectCommand.java
@@ -0,0 +1,34 @@
+package zeale.apps.stuff.app.console;
+
+import org.alixia.javalibrary.commands.StringCommand;
+
+public class ParsedObjectCommand<O> {
+	private final StringCommand cmd;
+	private final O data;
+
+	public String cmd() {
+		return cmd.command;
+	}
+
+	public String input() {
+		return cmd.inputText;
+	}
+
+	public String[] getArgs() {
+		return cmd.args;
+	}
+
+	public ParsedObjectCommand(StringCommand cmd, O data) {
+		this.cmd = cmd;
+		this.data = data;
+	}
+
+	public StringCommand getCmd() {
+		return cmd;
+	}
+
+	public O getData() {
+		return data;
+	}
+
+}
