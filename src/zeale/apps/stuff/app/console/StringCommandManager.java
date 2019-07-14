9fd264929bbea2fde193b276bf86206fa31c2590
Zeale <zeale@dusttoash.org>
2019-07-14 01:14:12 -0500

    Added StringCommandManager (cmd manager for std console input cmds)

---
+++ src/zeale/apps/stuff/app/console/StringCommandManager.java
@@ -0,0 +1,23 @@
+package zeale.apps.stuff.app.console;
+
+import org.alixia.javalibrary.commands.GenericCommandManager;
+import org.alixia.javalibrary.commands.processing.StringCommandParser;
+
+import zeale.apps.tools.console.std.StandardConsole.StandardConsoleUserInput;
+
+class StringCommandManager extends GenericCommandManager<ParsedObjectCommand<StandardConsoleUserInput>> {
+
+	private final StringCommandParser parser;
+
+	public StringCommandManager(StringCommandParser parser) {
+		this.parser = parser;
+	}
+
+	public StringCommandManager(String cmdChar) {
+		this(new StringCommandParser(cmdChar));
+	}
+
+	public boolean run(StandardConsoleUserInput data) {
+		return super.run(new ParsedObjectCommand<>(parser.parse(data.text), data));
+	}
+}
