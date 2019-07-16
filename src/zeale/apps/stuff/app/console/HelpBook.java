ab55c6100587402ff334e3855a5cc79c12fe0dfd
Zeale <zeale@dusttoash.org>
2019-07-16 02:20:29 -0500

    Fixed & Improved HelpBook printing by page

--- src/zeale/apps/stuff/app/console/HelpBook.java
+++ src/zeale/apps/stuff/app/console/HelpBook.java
@@ -23,38 +23,57 @@ class HelpBook {
 
 	}
 
+	public void addCommand(CommandHelp help) {
+		helps.add(help);
+	}
+
 	public void addCommand(String name, String description, String usage, String... aliases) {
 		helps.add(new CommandHelp(name, description, usage, aliases));
 	}
 
 	public void print(StyledPrintable printable, int page) throws HelpPageException {
-		int item = page - 1;
-		if (item < 0 || item * 3 > helps.size())
-			throw new HelpPageException(page, helps.size() / 3);
-		for (int i = item; i < item + 3; i++) {
+		int item = (page - 1) * 3;
+		if (item < 0 || item > helps.size())
+			throw new HelpPageException(page, (helps.size() + 2) / 3);
+
+		printable.print("Showing page ", Color.RED, true, false);
+		printable.print(page + "", Color.GOLD, true, false);
+		printable.print(" of help.", Color.RED, true, false);
+		printable.println();
+		for (int i = item; i < item + 3;) {
 			CommandHelp commandHelp = helps.get(i);
 
 			printable.print("Command: ", Color.RED);
-			printable.println(commandHelp.name, Color.GOLD);
-			printable.print("\t- Description: ", Color.RED);
+			printable.print(commandHelp.name, Color.GOLD, true, false);
+			printable.println();
+			printable.print("   - Description: ", Color.RED);
 			printable.print(commandHelp.description, Color.GOLD, false, true);
 			printable.println();
-			printable.print("\t- Usage: ", Color.RED);
+			printable.print("   - Usage: ", Color.RED);
 			printable.print(commandHelp.usage, Color.GOLD);
 			printable.println();
 			if (commandHelp.aliases.length > 0) {
-				printable.print("\t- Aliases: ", Color.RED);
+				printable.print("   - Aliases: ", Color.RED);
 				printable.print(commandHelp.aliases[0], Color.GOLD);
 				for (int j = 1; j < commandHelp.aliases.length; j++) {
 					printable.print(", ", Color.RED);
 					printable.print(commandHelp.aliases[j], Color.GOLD);
 				}
+				printable.println();
 			}
-			if (i >= helps.size()) {
+			if (++i >= helps.size()) {
 				printable.print("End of help reached.", Color.RED, true, false);
-				break;
+				return;
 			}
 		}
+		int maxPage = (helps.size() + 2) / 3;
+		if (page < maxPage) {
+			printable.print("Type ", Color.RED, true, false);
+			printable.print("help " + (page + 1), Color.GOLD, true, false);
+			printable.print(" to view the next page.", Color.RED, true, false);
+			printable.println();
+		}
+
 	}
 
 	public void print(StyledPrintable printable, String command, boolean allowAliases) {
