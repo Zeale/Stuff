e8d7cfb15f8cff73de15a109b13c246d436512cf
Zeale <zeale@dusttoash.org>
2019-07-21 18:22:49 -0500

    Added consumed property to Events

--- src/zeale/apps/stuff/api/chatroom/events/Event.java
+++ src/zeale/apps/stuff/api/chatroom/events/Event.java
@@ -4,6 +4,19 @@ import java.time.Instant;
 
 public class Event {
 	private final Instant timestamp;
+	private boolean consumed;
+
+	public boolean isConsumed() {
+		return consumed;
+	}
+
+	public void setConsumed(boolean consumed) {
+		this.consumed = consumed;
+	}
+
+	public void consume() {
+		setConsumed(true);
+	}
 
 	public Event(Instant timestamp) {
 		this.timestamp = timestamp;
