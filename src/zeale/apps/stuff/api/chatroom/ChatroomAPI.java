8d065c68dc6d49f988e07684da6f36a921e639b1
Zeale <zeale@dusttoash.org>
2019-07-21 00:08:55 -0500

    Added some docs for Chatroom API

--- src/zeale/apps/stuff/api/chatroom/ChatroomAPI.java
+++ src/zeale/apps/stuff/api/chatroom/ChatroomAPI.java
@@ -1,13 +1,45 @@
 package zeale.apps.stuff.api.chatroom;
 
+import zeale.apps.stuff.Stuff;
+
+/**
+ * This class (mostly) provides access to non-functional API features, such as
+ * the {@link #DEFAULT_CONNECTION_PORT default port that ChatroomServer's use}.
+ * 
+ * @author Zeale
+ *
+ */
 public final class ChatroomAPI {
+
+	/**
+	 * This is the address of "The Main Server," or, more specifically, my desktop.
+	 * When you run {@link Stuff} and launch the chatroom, by default, it will try
+	 * to connect to The Main Server. Since I wanted to make the API classes in
+	 * these packages more independent and since my desktop may go offline for
+	 * stupid reasons, the connection classes involved in this API are able to
+	 * connect to any given address, searching for or hosting a chatroom server.
+	 * This field is the default address.
+	 */
 	private final static String DEFAULT_CONNECTION_ADDRESS = "dusttoash.org";
+	/**
+	 * This is the default port that chatroom servers connect to.
+	 */
 	private final static int DEFAULT_CONNECTION_PORT = 42069;// Go figure why that was picked.
 
+	/**
+	 * Gets the {@link #DEFAULT_CONNECTION_ADDRESS default connection address}: (the
+	 * address of The Main Server).
+	 * 
+	 * @return The default connection address.
+	 */
 	public static String getDefaultConnectionAddress() {
 		return DEFAULT_CONNECTION_ADDRESS;
 	}
 
+	/**
+	 * @return The {@link #DEFAULT_CONNECTION_PORT default port} used by
+	 *         {@link ChatroomServer}s.
+	 */
 	public static int getDefaultConnectionPort() {
 		return DEFAULT_CONNECTION_PORT;
 	}
---
+++ src/zeale/apps/stuff/api/chatroom/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * <h1 style="font-size: 1.4em;text-align: center;color: red;">Chatroom's
+ * API</h1>
+ * <h4>Introduction</h4>
+ * <p>
+ * This package and its subpackages, collectively, serve as, the Chatroom
+ * application's API. They are referred to as <b style="color: red">The Chatroom
+ * API</b> throughout this API's documentation.
+ * </p>
+ * <p>
+ * This API provides interfaces for connecting to and hosting Chatroom servers,
+ * developing and using Chatroom clients, creating Chatroom bots, and more.
+ * </p>
+ * <p>
+ * TODO
+ */
+package zeale.apps.stuff.api.chatroom;
