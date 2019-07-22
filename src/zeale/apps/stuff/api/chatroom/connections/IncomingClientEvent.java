6d5028088ebb08dfe0de118d2d8fc9b8aa8cd638
Zeale <zeale@dusttoash.org>
2019-07-22 18:07:07 -0500

    Added IncomingClientEvent

---
+++ src/zeale/apps/stuff/api/chatroom/connections/IncomingClientEvent.java
@@ -0,0 +1,27 @@
+package zeale.apps.stuff.api.chatroom.connections;
+
+import java.time.Instant;
+
+import org.alixia.javalibrary.networking.sockets.Client;
+
+import zeale.apps.stuff.api.chatroom.events.EventType;
+
+public class IncomingClientEvent extends ChatroomServerEvent {
+	private final Client client;
+	public static final EventType<IncomingClientEvent> INCOMING_CLIENT_EVENT = new EventType<>(CHATROOM_SERVER_EVENT);
+
+	public Client getClient() {
+		return client;
+	}
+
+	public IncomingClientEvent(ChatroomServer server, Client client) {
+		super(server);
+		this.client = client;
+	}
+
+	public IncomingClientEvent(Instant timestamp, ChatroomServer server, Client client) {
+		super(timestamp, server);
+		this.client = client;
+	}
+
+}
