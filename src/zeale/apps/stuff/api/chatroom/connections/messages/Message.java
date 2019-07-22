56ccd9d06c2bcc251a28f2094143b48f793cc85f
Zeale <zeale@dusttoash.org>
2019-07-22 03:46:21 -0500

    Added Message cls & package (for server/client msgs)

---
+++ src/zeale/apps/stuff/api/chatroom/connections/messages/Message.java
@@ -0,0 +1,12 @@
+package zeale.apps.stuff.api.chatroom.connections.messages;
+
+import java.io.Serializable;
+
+public abstract class Message implements Serializable {
+
+	/**
+	 * SUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
