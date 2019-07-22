a6b5629f8deb609590b35228f9aebf82e64469d6
Zeale <zeale@dusttoash.org>
2019-07-22 03:46:29 -0500

    Added EndConnectionMessage

---
+++ src/zeale/apps/stuff/api/chatroom/connections/messages/EndConnectionMessage.java
@@ -0,0 +1,20 @@
+package zeale.apps.stuff.api.chatroom.connections.messages;
+
+public class EndConnectionMessage extends Message {
+
+	/**
+	 * SUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final String reason;
+
+	public EndConnectionMessage(String reason) {
+		this.reason = reason;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+}
