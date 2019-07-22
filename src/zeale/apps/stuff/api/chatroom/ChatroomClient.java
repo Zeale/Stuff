e2b3da5fd5acca9f7b5876d76677d6d1fab64a22
Zeale <zeale@dusttoash.org>
2019-07-21 21:51:35 -0500

    Added ChatroomClient class (NF)

---
+++ src/zeale/apps/stuff/api/chatroom/ChatroomClient.java
@@ -0,0 +1,11 @@
+package zeale.apps.stuff.api.chatroom;
+
+import org.alixia.javalibrary.networking.sockets.Client;
+
+public class ChatroomClient {
+	private Client client;
+
+	public ChatroomClient() {
+		// TODO Auto-generated constructor stub
+	}
+}
