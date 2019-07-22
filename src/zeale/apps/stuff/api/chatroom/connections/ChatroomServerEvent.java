cbc8f52af0f6623381220395c9b1c0f16f7b2d8d
Zeale <zeale@dusttoash.org>
2019-07-22 18:16:11 -0500

    Added ChatroomServerEvent

---
+++ src/zeale/apps/stuff/api/chatroom/connections/ChatroomServerEvent.java
@@ -0,0 +1,25 @@
+package zeale.apps.stuff.api.chatroom.connections;
+
+import java.time.Instant;
+
+import zeale.apps.stuff.api.chatroom.events.Event;
+import zeale.apps.stuff.api.chatroom.events.EventType;
+
+public class ChatroomServerEvent extends Event {
+	public static final EventType<ChatroomServerEvent> CHATROOM_SERVER_EVENT = new EventType<>(EventType.EVENT);
+	private final ChatroomServer server;
+
+	public ChatroomServer getServer() {
+		return server;
+	}
+
+	public ChatroomServerEvent(ChatroomServer server) {
+		this.server = server;
+	}
+
+	public ChatroomServerEvent(Instant timestamp, ChatroomServer server) {
+		super(timestamp);
+		this.server = server;
+	}
+
+}
