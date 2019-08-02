cdfb135d16b1ab1ae1bdaa16bee6d2c792b760b7
Zeale <zeale@dusttoash.org>
2019-08-01 23:45:56 -0500

    Updated ChatroomClient class

--- src/zeale/apps/stuff/api/chatroom/connections/client/ChatroomClient.java
+++ src/zeale/apps/stuff/api/chatroom/connections/client/ChatroomClient.java
@@ -1,30 +1,57 @@
 package zeale.apps.stuff.api.chatroom.connections.client;
 
+import java.io.Closeable;
 import java.io.IOException;
+import java.io.Serializable;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import org.alixia.javalibrary.networking.sockets.Client;
+import org.alixia.javalibrary.util.Box;
 
-import zeale.apps.stuff.api.chatroom.events.Event;
-import zeale.apps.stuff.api.chatroom.events.EventHandler;
-import zeale.apps.stuff.api.chatroom.events.EventManager;
-import zeale.apps.stuff.api.chatroom.events.EventType;
+//import zeale.apps.stuff.api.chatroom.events.Event;
+//import zeale.apps.stuff.api.chatroom.events.EventHandler;
+//import zeale.apps.stuff.api.chatroom.events.EventManager;
+//import zeale.apps.stuff.api.chatroom.events.EventType;
 
-public class ChatroomClient {
+public class ChatroomClient implements Closeable {
 	private Client client;
 
-	private final EventManager<Event> eventManager = new EventManager<>();
+	// TODO Update (events)
 
-	public EventManager<Event> getEventManager() {
-		return eventManager;
+	public void send(Serializable item) throws IOException {
+		client.send(item);
 	}
 
-	public <T extends Event> void register(EventType<T> type, EventHandler<? super T> handler) {
-		eventManager.register(type, handler);
+	public IOException trySend(Serializable item) {
+		return client.trySend(item);
 	}
 
-	public <T extends Event> void unregsiter(EventType<T> type, EventHandler<? super T> handler) {
-		eventManager.unregsiter(type, handler);
+	public IOException testConnection() {
+		return client.testConnection();
 	}
+
+	public Box<Serializable> read(int millisTimeout) throws ClassNotFoundException, IOException {
+		return client.read(millisTimeout);
+	}
+
+	public Serializable read() throws ClassNotFoundException, IOException {
+		return client.read();
+	}
+
+//	private final EventManager<Event> eventManager = new EventManager<>();
+//
+//	public EventManager<Event> getEventManager() {
+//		return eventManager;
+//	}
+//
+//	public <T extends Event> void register(EventType<T> type, EventHandler<? super T> handler) {
+//		eventManager.register(type, handler);
+//	}
+//
+//	public <T extends Event> void unregsiter(EventType<T> type, EventHandler<? super T> handler) {
+//		eventManager.unregsiter(type, handler);
+//	}
 
 	private final int port;
 	private final String host;
@@ -33,9 +60,49 @@ public class ChatroomClient {
 		this.host = host;
 		this.port = port;
 	}
-	
-	private void reinstateClient() throws IOException {
-		
+
+	public int getPort() {
+		return port;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	protected Client generateClient(String host, int port) throws UnknownHostException, IOException {
+		return new Client(new Socket(host, port));
+	}
+
+	public void launch() throws IOException {
+		if (client != null)
+			throw new IllegalStateException("Chatroom client already started.");
+		reinstateClient();
+	}
+
+	/**
+	 * If an exception occurs while trying to {@link #generateClient(String, int)
+	 * generate a new client}.
+	 * 
+	 * @throws IOException The {@link IOException} that occurred.
+	 */
+	public void reinstateClient() throws IOException {
+		if (client != null)
+			client.tryClose();
+		client = generateClient(host, port);
+	}
+
+	@Override
+	public void close() throws IOException {
+		client.close();
+	}
+
+	public IOException tryClose() {
+		try {
+			close();
+			return null;
+		} catch (IOException e) {
+			return e;
+		}
 	}
 
 }
