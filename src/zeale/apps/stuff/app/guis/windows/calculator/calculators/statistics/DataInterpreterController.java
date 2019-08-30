8216664e26e4e88427a8ae7f4328fdb9f0bb0aaa
Zeale <zeale@dusttoash.org>
2019-08-29 21:35:27 -0500

    Add controller class for DataInterpreter

---
+++ src/zeale/apps/stuff/app/guis/windows/calculator/calculators/statistics/DataInterpreterController.java
@@ -0,0 +1,10 @@
+package zeale.apps.stuff.app.guis.windows.calculator.calculators.statistics;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.TilePane;
+
+public class DataInterpreterController {
+	private @FXML TextArea dataInput;
+	private @FXML TilePane detectedData;
+}
