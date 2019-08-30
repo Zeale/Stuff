f2fb4ee32e0808473ed0d438c8a46ee76d255553
Zeale <zeale@dusttoash.org>
2019-08-30 13:49:47 -0500

    Add FXML data to controller

--- src/zeale/apps/stuff/app/guis/windows/calculator/calculators/statistics/DataInterpreterController.java
+++ src/zeale/apps/stuff/app/guis/windows/calculator/calculators/statistics/DataInterpreterController.java
@@ -1,10 +1,24 @@
 package zeale.apps.stuff.app.guis.windows.calculator.calculators.statistics;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.TilePane;
 
 public class DataInterpreterController {
 	private @FXML TextArea dataInput;
 	private @FXML TilePane detectedData;
+
+	private @FXML TableView<String> statisticsTable;
+	private @FXML TableColumn<String, String> properties, values;
+	
+	private @FXML void initialize() {
+		
+	}
+
+	private @FXML void calculateStats() {
+
+	}
+
 }
