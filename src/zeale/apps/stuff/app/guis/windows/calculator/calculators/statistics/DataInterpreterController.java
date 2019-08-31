51692ec03f2b3e70f10c525a5af7e4d6e24be6ef
Zeale <zeale@dusttoash.org>
2019-08-31 12:57:06 -0500

    Add functionality to output boxes (DataInterpreter)

--- src/zeale/apps/stuff/app/guis/windows/calculator/calculators/statistics/DataInterpreterController.java
+++ src/zeale/apps/stuff/app/guis/windows/calculator/calculators/statistics/DataInterpreterController.java
@@ -1,7 +1,10 @@
 package zeale.apps.stuff.app.guis.windows.calculator.calculators.statistics;
 
+import org.alixia.javalibrary.streams.CharacterStream;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.TableCell;
@@ -9,13 +12,18 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.TilePane;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.util.Callback;
+import main.alixia.javalibrary.javafx.tools.FXTools;
 
 public class DataInterpreterController {
 	private @FXML TextArea discreteDataInput, continuousDataInput;
-	private @FXML TilePane discreteDetectedData, continuousDetectedData;
+	private @FXML FlowPane discreteDetectedData, continuousDetectedData;
 
 	private @FXML TableView<Property> discreteTable, continuousTable;
 	private @FXML TableColumn<Property, String> discreteProps, continuousProps, discreteVals, continuousVals;
@@ -24,6 +32,55 @@ public class DataInterpreterController {
 			contProps = PropertyTemplate.props(ContProperty.values());
 
 	private @FXML void initialize() {
+
+		Background color = FXTools.getBackgroundFromColor(new Color(0, 0, 0, 0.2));
+
+		class InputHandler implements EventHandler<KeyEvent> {
+			private final TextArea input;
+			private final FlowPane output;
+
+			public InputHandler(TextArea input, FlowPane output) {
+				(this.input = input).setOnKeyReleased(this);
+				this.output = output;
+			}
+
+			@Override
+			public void handle(KeyEvent event) {
+				CharacterStream str = CharacterStream.from(input.getText());
+				StringBuilder curr = new StringBuilder();
+				int n;
+				output.getChildren().clear();
+				while ((n = str.next()) != -1)
+					if (Character.isWhitespace(n)) {
+						while (Character.isWhitespace(n = str.next()))
+							;
+
+						Text data = new Text(curr.toString());
+						StackPane box = new StackPane(data);
+						box.setBackground(color);
+						data.setFill(Color.GOLD);
+						output.getChildren().add(box);
+
+						if (n == -1)
+							return;
+						else
+							curr = new StringBuilder().append((char) n);
+					} else
+						curr.append((char) n);
+				if (curr.length() != 0) {
+					Text data = new Text(curr.toString());
+					StackPane box = new StackPane(data);
+					box.setBackground(color);
+					data.setFill(Color.GOLD);
+					output.getChildren().add(box);
+				}
+
+			}
+
+		}
+
+		new InputHandler(discreteDataInput, discreteDetectedData);
+		new InputHandler(continuousDataInput, continuousDetectedData);
 
 		discreteTable.getItems().addAll(discProps);
 		continuousTable.getItems().addAll(contProps);
