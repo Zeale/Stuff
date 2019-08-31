02a310f6d7a3ffb88834d7da055a5811afd9c65c
Zeale <zeale@dusttoash.org>
2019-08-31 15:38:13 -0500

    Add ContinuousValue cls

---
+++ src/zeale/apps/stuff/app/guis/windows/calculator/calculators/statistics/ContinuousValue.java
@@ -0,0 +1,14 @@
+package zeale.apps.stuff.app.guis.windows.calculator.calculators.statistics;
+
+public class ContinuousValue {
+	private final double value;
+
+	public ContinuousValue(double value) {
+		this.value = value;
+	}
+
+	public double getValue() {
+		return value;
+	}
+	
+}
