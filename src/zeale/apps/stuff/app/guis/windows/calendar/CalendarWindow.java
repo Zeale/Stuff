4d7c7a196f188154f1fe6982b469cf62a8d2ad6b
Zeale <zeale@dusttoash.org>
2019-09-04 13:28:32 -0500

    Add CalendarGUI

--- src/zeale/apps/stuff/app/guis/windows/calendar/CalendarWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/calendar/CalendarWindow.java
@@ -1,7 +1,34 @@
 package zeale.apps.stuff.app.guis.windows.calendar;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
 
 public class CalendarWindow extends Window {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
+		FXMLLoader loader = new FXMLLoader(CalendarWindow.class.getResource("CalendarGUI.fxml"));
+		loader.setController(this);
+
+		try {
+			Parent root = loader.load();
+			root.getStylesheets().addAll(properties.popButtonStylesheet.get(), properties.themeStylesheet.get());
+			stage.setScene(new Scene(root));
+		} catch (IOException e) {
+			throw new WindowLoadFailureException(e);
+		}
+	}
 
 }
