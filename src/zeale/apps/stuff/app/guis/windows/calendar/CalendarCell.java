e36896791805a33e418d7e1c2f0cc220394097b7
Zeale <zeale@dusttoash.org>
2019-09-06 16:57:56 -0500

    Add CalendarCell background color methods

--- src/zeale/apps/stuff/app/guis/windows/calendar/CalendarCell.java
+++ src/zeale/apps/stuff/app/guis/windows/calendar/CalendarCell.java
@@ -8,8 +8,10 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import zeale.applicationss.notesss.utilities.Utilities;
 
@@ -17,6 +19,36 @@ class CalendarCell extends StackPane {
 	private final IntegerProperty number = new SimpleIntegerProperty();
 	private final Text text = new Text();
 	private final static DropShadow shadow = new DropShadow();
+
+	public static final double DEFAULT_CELL_BACKGROUND_OPACITY = 0.2;
+
+	private static final Color DEFAULT_DISABLED_CELL_BACKGROUND_COLOR = new Color(0.6, 0, 0,
+			DEFAULT_CELL_BACKGROUND_OPACITY);
+	private static final Background DEFAULT_DISABLED_CELL_BACKGROUND = Utilities
+			.getBackgroundFromColor(DEFAULT_DISABLED_CELL_BACKGROUND_COLOR);
+
+	/**
+	 * Sets the background color of this {@link CalendarCell} to be the given color
+	 * <em>with an opacity of <code>0.2</code></em>.
+	 * 
+	 * @param color The {@link Color} to set this {@link CalendarCell}'s background
+	 *              to.
+	 */
+	public void setBackgroundColor(Color color) {
+		setBackgroundColorStrict(color.deriveColor(0, 0, 0, 0.2));
+	}
+
+	/**
+	 * Sets the background of this {@link CalendarCell} to be a {@link Background}
+	 * derived from the given {@link Paint}.
+	 * 
+	 * @param color The {@link Paint} to make the background of this
+	 *              {@link CalendarCell}.
+	 */
+	public void setBackgroundColorStrict(Paint color) {
+		setBackground(Utilities.getBackgroundFromColor(color));
+	}
+
 	{
 		getChildren().add(text);
 		setAlignment(text, Pos.TOP_RIGHT);
@@ -44,8 +76,8 @@ class CalendarCell extends StackPane {
 			setEffect(null);
 			setStyle(style.value);
 		});
-		disabledProperty().addListener((observable, oldValue, newValue) -> setBackground(
-				newValue ? Utilities.getBackgroundFromColor(new Color(0.6, 0, 0, 0.2)) : null));
+		disabledProperty().addListener(
+				(observable, oldValue, newValue) -> setBackground(newValue ? DEFAULT_DISABLED_CELL_BACKGROUND : null));
 	}
 
 	public final IntegerProperty numberProperty() {
