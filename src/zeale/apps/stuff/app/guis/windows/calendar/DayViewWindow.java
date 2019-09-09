e19a5c73c2ad3e6df5eaa690a4d0a109b05639e9
Zeale <zeale@dusttoash.org>
2019-09-08 23:03:33 -0500

    Date view window require certain properties

--- src/zeale/apps/stuff/app/guis/windows/calendar/DayViewWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/calendar/DayViewWindow.java
@@ -1,15 +1,27 @@
 package zeale.apps.stuff.app.guis.windows.calendar;
 
+import java.time.LocalDate;
+
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.stage.Stage;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
 
 class DayViewWindow extends Window {
 
+	private final LocalDate date;
+	private final ObservableMap<LocalDate, ObservableList<CalendarEvent>> dates;
+
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
 
+	}
+
+	public DayViewWindow(ObservableMap<LocalDate, ObservableList<CalendarEvent>> dates, LocalDate date) {
+		this.dates = dates;
+		this.date = date;
 	}
 
 	@Override
