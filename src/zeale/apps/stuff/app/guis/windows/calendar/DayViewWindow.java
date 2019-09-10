e5445d99507b95b90ec50a74db47d8d93bb37b08
Zeale <zeale@dusttoash.org>
2019-09-09 22:11:47 -0500

    Ability to show standalone DayViewWindow

--- src/zeale/apps/stuff/app/guis/windows/calendar/DayViewWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/calendar/DayViewWindow.java
@@ -2,16 +2,19 @@ package zeale.apps.stuff.app.guis.windows.calendar;
 
 import java.time.LocalDate;
 
-import javafx.collections.ObservableList;
-import javafx.collections.ObservableMap;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
 import javafx.stage.Stage;
+import zeale.apps.stuff.Stuff;
 import zeale.apps.stuff.api.appprops.ApplicationProperties;
 import zeale.apps.stuff.api.guis.windows.Window;
 
 class DayViewWindow extends Window {
 
 	private final LocalDate date;
-	private final ObservableMap<LocalDate, ObservableList<CalendarEvent>> dates;
+	private final CalendarWindow window;
+	private final boolean showBackButton;
+	private @FXML Menu commandsMenu;
 
 	@Override
 	public void destroy() {
@@ -19,15 +22,29 @@ class DayViewWindow extends Window {
 
 	}
 
-	public DayViewWindow(ObservableMap<LocalDate, ObservableList<CalendarEvent>> dates, LocalDate date) {
-		this.dates = dates;
+	public DayViewWindow(CalendarWindow window, LocalDate date, boolean showBackButton) {
 		this.date = date;
+		this.window = window;
+		this.showBackButton = showBackButton;
+	}
+
+	private @FXML void initialize() {
+		if (!showBackButton)
+			commandsMenu.setVisible(false);
 	}
 
 	@Override
 	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
 		// TODO Auto-generated method stub
 
+	}
+
+	private @FXML void back() {
+		try {
+			Stuff.displayWindow(window);
+		} catch (WindowLoadFailureException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
