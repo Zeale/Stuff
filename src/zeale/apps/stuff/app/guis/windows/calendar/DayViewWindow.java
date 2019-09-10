f1b13d9f445ccdf80c7e3ee3c85421d7fccba3ba
Zeale <zeale@dusttoash.org>
2019-09-09 23:17:06 -0500

    DayViewWindow show method impl

--- src/zeale/apps/stuff/app/guis/windows/calendar/DayViewWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/calendar/DayViewWindow.java
@@ -1,8 +1,12 @@
 package zeale.apps.stuff.app.guis.windows.calendar;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 import zeale.apps.stuff.Stuff;
@@ -35,7 +39,15 @@ class DayViewWindow extends Window {
 
 	@Override
 	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
-		// TODO Auto-generated method stub
+		FXMLLoader loader = new FXMLLoader(DayViewWindow.class.getResource("DayViewGUI.fxml"));
+		loader.setController(this);
+		try {
+			Parent parent = loader.load();
+			parent.getStylesheets().addAll(properties.popButtonStylesheet.get(), properties.themeStylesheet.get());
+			stage.setScene(new Scene(loader.load()));
+		} catch (IOException e) {
+			throw new WindowLoadFailureException("Failed to show the Daily View window.", e);
+		}
 
 	}
 
