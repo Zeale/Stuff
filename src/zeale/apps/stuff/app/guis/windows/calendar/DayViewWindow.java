984bc87da74ea83dba305019f0073ca1e2143b57
Zeale <zeale@dusttoash.org>
2019-09-12 13:15:31 -0500

    Update DayViewWindow

--- src/zeale/apps/stuff/app/guis/windows/calendar/DayViewWindow.java
+++ src/zeale/apps/stuff/app/guis/windows/calendar/DayViewWindow.java
@@ -2,11 +2,19 @@ package zeale.apps.stuff.app.guis.windows.calendar;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 import zeale.apps.stuff.Stuff;
@@ -16,9 +24,40 @@ import zeale.apps.stuff.api.guis.windows.Window;
 class DayViewWindow extends Window {
 
 	private final LocalDate date;
+	private final ObservableMap<LocalDate, ObservableList<CalendarEvent>> events;
 	private final CalendarWindow window;
 	private final boolean showBackButton;
 	private @FXML Menu commandsMenu;
+
+	private @FXML PieChart eventBreakdown, taskBreakdown, dayProgress;
+	private final IntegerProperty finishedEvents = new SimpleIntegerProperty(),
+			unfinishedEvents = new SimpleIntegerProperty(), finishedTasks = new SimpleIntegerProperty(),
+			unfinishedTasks = new SimpleIntegerProperty();
+
+	private @FXML void refreshEvents() {
+		int fin = 0, un = 0;
+		LocalTime now = LocalTime.now();
+		for (CalendarEvent ce : events.get(date))
+			if ((ce.getEndTime() == null ? ce.getTime() : ce.getEndTime()).isBefore(now))
+				fin++;
+			else
+				un++;
+		finishedEvents.set(fin);
+		unfinishedEvents.set(un);
+	}
+
+	private @FXML void refreshTasks() {
+		// TODO
+	}
+
+	private final InvalidationListener eventListener = o -> refreshEvents();
+
+	private final MapChangeListener<LocalDate, ObservableList<CalendarEvent>> mapListener = change -> {
+		if (change.wasAdded())
+			change.getValueAdded().addListener(eventListener);
+		else
+			change.getValueRemoved().removeListener(eventListener);
+	};
 
 	@Override
 	public void destroy() {
@@ -26,15 +65,18 @@ class DayViewWindow extends Window {
 
 	}
 
-	public DayViewWindow(CalendarWindow window, LocalDate date, boolean showBackButton) {
-		this.date = date;
+	public DayViewWindow(CalendarWindow window, LocalDate date, boolean showBackButton,
+			ObservableMap<LocalDate, ObservableList<CalendarEvent>> events) {
 		this.window = window;
+		this.date = date;
+		(this.events = events).addListener(mapListener);
 		this.showBackButton = showBackButton;
 	}
 
 	private @FXML void initialize() {
 		if (!showBackButton)
 			commandsMenu.setVisible(false);
+
 	}
 
 	@Override
