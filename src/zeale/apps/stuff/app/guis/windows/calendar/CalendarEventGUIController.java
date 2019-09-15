b3cc6ac4820aa7379fe74f1dea8851626fc53e21
Zeale <zeale@dusttoash.org>
2019-09-14 23:22:27 -0500

    Add columns and update controller

--- src/zeale/apps/stuff/app/guis/windows/calendar/CalendarEventGUIController.java
+++ src/zeale/apps/stuff/app/guis/windows/calendar/CalendarEventGUIController.java
@@ -1,5 +1,19 @@
 package zeale.apps.stuff.app.guis.windows.calendar;
 
-public class CalendarEventGUIController {
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CalendarEventGUIController {
+	private @FXML TableView<CalendarEvent> eventTable;
+	private @FXML TableColumn<CalendarEvent, String> nameColumn, descriptionColumn;
+	private @FXML TableColumn<CalendarEvent, LocalDate> dateColumn;
+	private @FXML TableColumn<CalendarEvent, LocalTime> timeColumn, endTimeColumn;
+
+	private @FXML void initialize() {
+
+	}
 }
